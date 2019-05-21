d083af3c5a6cd13f4595157320bd673d6f1e064a
KevinSarav <38804180+KevinSarav@users.noreply.github.com>
2019-05-20 23:31:24 -0500

    Create Sock Merchant.java

---
+++ Sock Merchant.java
@@ -0,0 +1,60 @@
+//This is the Sock Merchant challenge. Here, I found a way to use both HashSets and HashMaps. The link to the challenge is below.
+//https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int count = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for(int i = 0; i < n; i++){
+            map.merge(ar[i], 1, Integer::sum);
+            set.add(ar[i]);
+        }
+
+        Iterator<Integer> iter = set.iterator();
+        while(iter.hasNext()){
+            count += (map.get(iter.next()) / 2);
+        }
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
